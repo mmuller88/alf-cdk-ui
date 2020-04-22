637794631b97bc56f01fe9ba3eac2632b2da832f
Martin Müller <damadden88@googlemail.com>
2020-04-22 18:21:52 +0200

    complete readme about workaround

--- src/App.tsx
+++ src/App.tsx
@@ -34,9 +34,9 @@ class App extends Component{
             } else {
               // url = proxyUrl + '/' + this.url
               request.headers.Authorization = `${jwt}`;
-              request.headers['Access-Control-Allow-Origin'] = '*'
-              request.headers['Access-Control-Allow-Methods'] = "DELETE, POST, GET, OPTIONS"
-              request.headers['Access-Control-Allow-Headers'] = "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+              // request.headers['Access-Control-Allow-Origin'] = '*'
+              // request.headers['Access-Control-Allow-Methods'] = "DELETE, POST, GET, OPTIONS"
+              // request.headers['Access-Control-Allow-Headers'] = "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
               // if (request.method === "OPTIONS"){
               //   w.WriteHeader(http.StatusOK)
               //   return
@@ -45,6 +45,12 @@ class App extends Component{
               console.log(`Body: ${request.body}`)
               return request;
             }
+          } }
+          responseInterceptor= { function(response) {
+              response.headers['Access-Control-Allow-Origin'] = '*'
+              response.headers['Access-Control-Allow-Methods'] = "DELETE, POST, GET, OPTIONS"
+              response.headers['Access-Control-Allow-Headers'] = "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+              return response;
           } }
 
         />
