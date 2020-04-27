24ae2eed20dbcec2032ffe8f10e0f546429849ef
Martin Müller <damadden88@googlemail.com>
2020-04-27 17:07:33 +0200

    take auth and permisison

--- src/App.tsx
+++ src/App.tsx
@@ -20,43 +20,43 @@ class App extends Component{
   render(): any {
     return (
       <div>
-        <SwaggerUI
+        {/* <SwaggerUI
           // url="https://api-explorer.h-o.dev.s3.eu-west-2.amazonaws.com/swagger.json"
           url="https://api-explorer.h-o.dev/swagger.json"
-          // docExpansion="list"
-          // // url="https://s3.eu-west-2.amazonaws.com/api-explorer.h-o.dev/swagger.json"
-          // requestInterceptor= { function(request) {
+          docExpansion="list"
+          // url="https://s3.eu-west-2.amazonaws.com/api-explorer.h-o.dev/swagger.json"
+          requestInterceptor= { function(request) {
 
-          //   // console.log(`UserName: ${userName}`)
-          //   // console.log(`UserName: ${userName}`)
-          //   // Allow developers to set a bearertoken since
-          //   // const bearerToken = sessionStorage.getItem('bearerToken');
-          //   // if (!jwt) {
-          //   //   alert(`From the console, please run: \nsessionStorage.setItem('accessToken', 'insert a real access token id here')`);
-          //   //   return request;
-          //   // } else {
-          //     // url = proxyUrl + '/' + this.url
-          //     request.headers.Authorization = `${jwt}`;
-          //     request.headers['Access-Control-Allow-Origin'] = '*'
-          //     request.headers['Access-Control-Allow-Methods'] = "DELETE, POST, GET, OPTIONS"
-          //     request.headers['Access-Control-Allow-Headers'] = "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-          //     // if (request.method === "OPTIONS"){
-          //     //   w.WriteHeader(http.StatusOK)
-          //     //   return
-          //     // }
-          //     // request.parameters.alfUserId = `${userName}`;
-          //     // console.log(`Body: ${request.body}`)
-          //     return request;
-          //   // }
-          // } }
-          // responseInterceptor= { function(response) {
-          //     response.headers['Access-Control-Allow-Origin'] = '*'
-          //     response.headers['Access-Control-Allow-Methods'] = "DELETE, POST, GET, OPTIONS"
-          //     response.headers['Access-Control-Allow-Headers'] = "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-          //     return response;
-          // } }
+            // console.log(`UserName: ${userName}`)
+            // console.log(`UserName: ${userName}`)
+            // Allow developers to set a bearertoken since
+            // const bearerToken = sessionStorage.getItem('bearerToken');
+            // if (!jwt) {
+            //   alert(`From the console, please run: \nsessionStorage.setItem('accessToken', 'insert a real access token id here')`);
+            //   return request;
+            // } else {
+              // url = proxyUrl + '/' + this.url
+              request.headers.Authorization = `${jwt}`;
+              request.headers['Access-Control-Allow-Origin'] = '*'
+              request.headers['Access-Control-Allow-Methods'] = "DELETE, POST, GET, OPTIONS"
+              request.headers['Access-Control-Allow-Headers'] = "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+              // if (request.method === "OPTIONS"){
+              //   w.WriteHeader(http.StatusOK)
+              //   return
+              // }
+              // request.parameters.alfUserId = `${userName}`;
+              // console.log(`Body: ${request.body}`)
+              return request;
+            // }
+          } }
+          responseInterceptor= { function(response) {
+              response.headers['Access-Control-Allow-Origin'] = '*'
+              response.headers['Access-Control-Allow-Methods'] = "DELETE, POST, GET, OPTIONS"
+              response.headers['Access-Control-Allow-Headers'] = "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+              return response;
+          } }
 
-        />
+        /> */}
       </div>
     )
   }
