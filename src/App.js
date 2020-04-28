7b011553357d81521a272eed480fbd4c114cf223
Martin Müller <damadden88@googlemail.com>
2020-04-28 18:01:13 +0200

    init docker compose stuff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
-// import SwaggerUI from 'swagger-ui';
+import './App.css'
+import SwaggerUI from 'swagger-ui';
+import "swagger-ui-react/swagger-ui.css"
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth }from 'aws-amplify';
 import aws_exports from './aws-exports';
-// import { CognitoUser } from 'amazon-cognito-identity-js';
-import SwaggerUI from 'swagger-ui-react';
-// import ReactDOM from 'react-dom';
-import 'swagger-ui-react/swagger-ui.css';
+
 Amplify.configure(aws_exports);
 
-export default App = () => <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />
+var jwt;
+var userName;
 
-// export default withAuthenticator(App, true);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      definitionLink: "https://api-explorer.h-o.dev/swagger.json",
+    }
+
+    console.log("baue");
+
+    Auth.currentAuthenticatedUser({
+        bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+    }).then(authUser =>{
+      console.log(authUser)
+      userName = authUser.username;
+    } )
+    .catch(err => console.log(err));
+
+    Auth.currentSession().then(res=>{
+      let accessToken = res.getAccessToken()
+      jwt = accessToken.getJwtToken()
+      //You can print them to see the full objects
+      console.log(`myAccessToken: ${JSON.stringify(accessToken)}`)
+      console.log(`myJwt: ${jwt}`)
+    })
+  }
+
+  componentDidMount() {
+    SwaggerUI({
+      domNode: document.getElementById("api-data"),
+      url: this.state.definitionLink
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div id="api-data" />
+      </div>
+    );
+  }
+}
+
+export default withAuthenticator(App, true);
+
