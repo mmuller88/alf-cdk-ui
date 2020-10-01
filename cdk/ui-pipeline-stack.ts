dc82169542c9baece34f619f7f082962e1ade617
martin.mueller <m.mueller@unimed.de>
2020-10-01 10:23:09 +0200

    feat: update gitignore und dpendenies

--- cdk/ui-pipeline-stack.ts
+++ cdk/ui-pipeline-stack.ts
@@ -80,6 +80,7 @@ export class UIPipelineStack extends Stack {
       synthAction: SimpleSynthAction.standardNpmSynth({
         sourceArtifact,
         cloudAssemblyArtifact,
+        installCommand: `make install && npm install -g aws-cdk@${props.cdkVersion}`,
         synthCommand: 'make cdksynthprod',
         // We need a build step to compile the TypeScript Lambda
         buildCommand: 'make build && make cdkbuild'
