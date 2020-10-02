d62e7ae9805f910f86ab2cd10c6d68f8c218b6d7
martin.mueller <m.mueller@unimed.de>
2020-10-02 08:06:48 +0200

    Add new style

--- cdk/ui-pipeline-stack.ts
+++ cdk/ui-pipeline-stack.ts
@@ -60,10 +60,10 @@ export class UIPipelineStack extends Stack {
         sourceArtifact,
         cloudAssemblyArtifact,
         installCommand: `make install && npm install -g aws-cdk@${props.cdkVersion}`,
-        synthCommand: 'make cdksynthprod',
+        // synthCommand: 'make cdksynthprod',
         // subdirectory: 'cdk',
         // We need a build step to compile the TypeScript Lambda
-        buildCommand: 'make build && make cdkbuild',
+        // buildCommand: 'make build && make cdkbuild',
       }),
     });
 
