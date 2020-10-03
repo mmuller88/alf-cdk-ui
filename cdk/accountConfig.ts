ab58b594dd9a818a3f2fdbd5bbf1c09f7dadc45d
martin.mueller <m.mueller@unimed.de>
2020-10-03 07:48:51 +0200

    Add dev ui stack in eu-central-1

--- cdk/accountConfig.ts
+++ cdk/accountConfig.ts
@@ -17,7 +17,7 @@ export const devAccount = {
   stage: 'dev',
   domainName: 'dev.alfpro.net',
   subDomain: 'app',
-  acmCertRef: 'arn:aws:acm:us-east-1:981237193288:certificate/62010fca-125e-4780-8d71-7d745ff91789',
+  acmCertRef: 'arn:aws:acm:us-east-1:981237193288:certificate/0c2cc2a7-b0aa-4201-8d9b-dc7c23154676',
   hostedZoneId: 'Z036396421QYOR6PI3CPX',
   zoneName: 'dev.alfpro.net.',
 }
