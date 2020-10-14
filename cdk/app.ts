da6ec5710f059cd0dfdc882838fc63fc20cc1a4b
martin.mueller <m.mueller@unimed.de>
2020-10-14 08:12:35 +0200

    fix: use new pipe + staging dev and prod

--- cdk/app.ts
+++ cdk/app.ts
@@ -2,25 +2,58 @@
 import { UIStack } from './ui-stack';
 import { name } from './package.json';
 import { PipelineApp } from 'alf-cdk-app-pipeline/pipeline-app';
+import { sharedDevAccountProps, sharedProdAccountProps } from 'alf-cdk-app-pipeline/accountConfig';
 
 
 // tslint:disable-next-line: no-unused-expression
 new PipelineApp({
   branch: 'master',
   repositoryName: name,
+  accounts: [
+    {
+      id: '981237193288',
+      region: 'eu-central-1',
+      stage: 'dev',
+    },
+    {
+      id: '981237193288',
+      region: 'us-east-1',
+      stage: 'prod',
+    },
+  ],
+  buildAccount: {
+    id: '981237193288',
+    region: 'eu-central-1',
+    stage: 'dev',
+  },
   customStack: (scope, account) => {
+    const stageProps = {
+      ...(account.stage === 'dev' ? {
+        domainName: sharedDevAccountProps.domainName,
+        acmCertRef: sharedDevAccountProps.acmCertRef,
+        subDomain: sharedDevAccountProps.subDomain,
+        hostedZoneId: sharedDevAccountProps.hostedZoneId,
+        zoneName: sharedDevAccountProps.zoneName,
+      } : { // prod stage
+        domainName: sharedProdAccountProps.domainName,
+        acmCertRef: sharedProdAccountProps.acmCertRef,
+        subDomain: sharedProdAccountProps.subDomain,
+        hostedZoneId: sharedProdAccountProps.hostedZoneId,
+        zoneName: sharedProdAccountProps.zoneName,
+      })
+    };
     // console.log('echo = ' + JSON.stringify(account));
     return new UIStack(scope, `${name}-${account.stage}`, {
       stackName: `${name}-${account.stage}`,
       stage: account.stage,
-      domainName: account.domainName,
-      acmCertRef: account.acmCertRef,
-      subDomain: account.subDomain,
-      hostedZoneId: account.hostedZoneId,
-      zoneName: account.zoneName,
+      domainName: stageProps.domainName,
+      acmCertRef: stageProps.acmCertRef,
+      subDomain: stageProps.subDomain,
+      hostedZoneId: stageProps.hostedZoneId,
+      zoneName: stageProps.zoneName,
     })
   },
-  testCommands: [
+  testCommands: (_) => [
     // Use 'curl' to GET the given URL and fail if it returns an error
     'curl -Ssf $domainName',
     'echo done!!!',
